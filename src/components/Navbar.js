d7abece4b67ddb4f351952509197bd25db812a44
arshu18 <arshadarif186@gmail.com>
2022-04-29 02:40:44 +0530

    Made Navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
 // Components
@@ -9,14 +9,59 @@ import "./Navbar.css"
 
 const Navbar = () => {
     const [displayModal, setDisplayModal] = useState(false)
+    const [displayHamburger, setDisplayHamburger] = useState(false)
+    const [displayTabs, setDisplayTabs] = useState(true)
+    const [displayVerticalNav, setDisplayVerticalNav] = useState(false)
+    
+    useEffect(() => {
+        const resizeFn = () => {
+            if(window.innerWidth <= 600){
+                setDisplayHamburger(true)
+                setDisplayTabs(false)
+            }
+            else{
+                setDisplayHamburger(false)
+                setDisplayTabs(true)
+                setDisplayVerticalNav(false)
+            }
+        }
+        
+        window.addEventListener("resize", resizeFn)
+        return () => window.removeEventListener("resize", resizeFn)
+    }, [])
     
     return (
         <>
             { displayModal && <NewTask setDisplayModal={setDisplayModal} /> }
             <nav className="Navbar">
                 <Link to="/" className="Navbar-links">Todo List</Link>
-                <h4 className="Navbar-Newtask" onClick={() => setDisplayModal(true)}>New Task</h4>
+                {
+                    displayTabs && (
+                        <section className="Navbar-navigationContainer">
+                            <Link to="/" className="Navbar-Upcoming">Upcoming Tasks</Link>
+                            <h4 className="Navbar-Newtask" onClick={() => setDisplayModal(true)}>New Task</h4>
+                        </section>
+                    )
+                }
+                {
+                    displayHamburger && (
+                        <div className="Navbar-Hamburger" onClick={() => setDisplayVerticalNav(prevState => !prevState)}>
+                            <div className="Navbar-line"></div>
+                            <div className="Navbar-line"></div>
+                            <div className="Navbar-line"></div>
+                        </div>
+                    )
+                }
             </nav>
+            {
+                displayVerticalNav && (
+                    <section className="Navbar-vertical">
+                        <Link to="/" className="Navbar-Upcoming">Upcoming Tasks</Link>
+                        <h4 className="Navbar-Newtask-vertical" onClick={() => setDisplayModal(true)}>New Task</h4>
+                    </section>
+                )
+            }
+            
         </>
         
     )
